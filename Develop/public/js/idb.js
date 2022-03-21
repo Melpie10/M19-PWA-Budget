a9233b81fd82ba35f75eae2ace0f55b8eb0dfae5
melany14 <melany14@gmail.com>
2022-03-21 19:42:48 -0400

    made changes to idb

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -3,12 +3,12 @@ let db;
 // establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
 const request = indexedDB.open('budget_tracker', 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+// this event will emit if the database version changes (non-existent to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_budget', { autoIncrement: true });
+    // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
   // upon a successful 
@@ -16,9 +16,9 @@ request.onsuccess = function(event) {
     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
 
-    // check if app is online, if yes run uploadBudget() function to send all local db data to api
+    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
     if (navigator.onLine) {
-      uploadBudget();
+      uploadTransaction();
     }
 };
 
@@ -27,25 +27,25 @@ request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new budget and there's no internet connection
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    // access the object store for `new_budget`
-    const budgetObjectStore = transaction.objectStore('new_budget');
+    // access the object store for `new_transaction`
+    const budgetObjectStore = transaction.objectStore('new_transaction');
 
     // add record to your store with add method
     budgetObjectStore.add(record);
     }
 
 
-function uploadBudget() {
+function uploadTransaction() {
     // open a transaction on your db
-    const transaction = db.transaction(['new_budget'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
 
     // access your object store
-    const budgetObjectStore = transaction.objectStore('new_budget');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
 
     // get all records from store and set to a variable
     // upon a successful .getAll() execution, run this function
@@ -66,13 +66,13 @@ function uploadBudget() {
             throw new Error(serverResponse);
             }
             // open one more transaction
-            const transaction = db.transaction(['new_budget'], 'readwrite');
-            // access the new_budget object store
-            const budgetObjectStore = transaction.objectStore('new_budget');
+            const transaction = db.transaction(['new_transaction'], 'readwrite');
+            // access the new_transaction object store
+            const budgetObjectStore = transaction.objectStore('new_transaction');
             // clear all items in your store
             budgetObjectStore.clear();
 
-            alert('All saved budget has been submitted!');
+            alert('All saved transactions has been submitted!');
         })
         .catch(err => {
             console.log(err);
@@ -82,4 +82,4 @@ function uploadBudget() {
 }
 
 // listen for app coming back online
-window.addEventListener('online', uploadBudget);
+window.addEventListener('online', uploadTransaction);
