4f2068180624a3b24795ae0bad3eb30cd6304e99
melany14 <melany14@gmail.com>
2022-03-22 21:12:05 -0400

    changes to service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const RUNTIME_CACHE = "data-cache-" + VERSION;
 
 const FILES_TO_CACHE = [
     "./index.html",
@@ -50,14 +51,14 @@ self.addEventListener('activate', function(e) {
     });
 
 self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
+    console.log('fetch request : ' + e.request.url);
     e.respondWith(
         caches.match(e.request).then(function (request) {
         if (request) { // if cache is available, respond with cache
-            console.log('responding with cache : ' + e.request.url)
+            console.log('responding with cache : ' + e.request.url);
             return request
         } else {       // if there are no cache, try fetching request
-            console.log('file is not cached, fetching : ' + e.request.url)
+            console.log('file is not cached, fetching : ' + e.request.url);
             return fetch(e.request)
         }
     
@@ -65,4 +66,4 @@ self.addEventListener('fetch', function (e) {
         // return request || fetch(e.request)
         })
     )
-    })
+    });
